9b81157ee34fec0353c38307f422fc8416089ade
SimonVoyer <38772486+SimonVoyer@users.noreply.github.com>
2019-10-20 15:26:05 -0400

    minor update - variable name change

--- Source.cpp
+++ Source.cpp
@@ -4,17 +4,17 @@
 
 int main() {
 	HavelHakimi havelHakimi;
-	std::vector<int> graph;
+	std::vector<int> sequence;
 	std::string input;
 	std::cout << "Please enter a sequence of integers to verify whether it is graphical. Enter d when you are done.\n";
 	std::cin >> input;
 
 	while (input != "d" && input != "D"){
-		graph.push_back(stoi(input));
+		sequence.push_back(stoi(input));
 		std::cin >> input;
 	}
 
-	bool isGraphical = havelHakimi.graphRealization(graph);
+	bool isGraphical = havelHakimi.graphRealization(sequence);
 
 	if (isGraphical) {
 		std::cout << "The sequence is graphical.";
